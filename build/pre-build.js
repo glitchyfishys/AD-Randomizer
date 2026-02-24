9128aa41dcc89dbe01eeda6776a4459b92b2caba
glitchyfishys <glitchyfishys@gmail.com>
2026-02-24 21:45:39 +1300

    why...

--- build/pre-build.js
+++ build/pre-build.js
@@ -5,7 +5,7 @@ import browserslist from "browserslist-useragent-regexp";
 
 const userAgentRegExp = browserslist.getUserAgentRegExp({ allowHigherVersions: true });
 const checkFunction = `export const supportedBrowsers = ${userAgentRegExp};`;
-fs.writeFileSync(path.resolve(import.meta.dirname, "/src/supported-browsers.js"), checkFunction);
+fs.writeFileSync(path.resolve(import.meta.dirname, "../src/supported-browsers.js"), checkFunction);
 
 const firebaseConfig = process.env.FIREBASE_CONFIG;
 if (firebaseConfig) {
